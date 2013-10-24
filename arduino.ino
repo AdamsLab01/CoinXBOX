116bcf99d35f7c98186bbc4a42a0cc84051eb9bc
rocketboy001 <adam@adambyers.com>
2013-10-24 13:44:25 -0700

    license updates

--- arduino.ino
+++ arduino.ino
@@ -6,7 +6,22 @@ specific about the code and it should work swimmingly with any device that outpu
 
 For more information see - http://awaitinginspiration.com or ping adam@adambyers.com
 
-All code (except external libraries and third party code) is published under the MIT License.
+Coin Operated XBOX
+Copyright (C) 2013 Adam Byers
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 **********************************************************************************************************************/
 
